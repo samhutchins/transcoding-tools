12ee456ba733a2d7b8747f1c23adbfb73ebb06b5
Sam H <samhutchins@users.noreply.github.com>
2020-11-09 15:36:37 +0000

    Allow excluding all audio/subtitles when remuxing

--- remux.py
+++ remux.py
@@ -28,8 +28,10 @@ Output options:
 Remux options:
 -a, --select-audio TRACK [TRACK...]
                     select audio tracks by index to be included
+    --an            Disable audio output
 -s, --select-subtitle TRACK [TRACK...]
                     select subtitle tracks by index to be included
+    --sn            Disable subtitle output
 -f, --force-subtitle TRACK
                     select a subtitle track to be forced
 
@@ -45,8 +47,10 @@ def main():
     parser.add_argument("file", nargs="?")
     parser.add_argument("--inspect", action="store_true")
     parser.add_argument("--dry-run", action="store_true")
-    parser.add_argument("-a", "--select-audio", metavar="TRACK", nargs="+", type=int)
-    parser.add_argument("-s", "--select-subtitle", metavar="TRACK", nargs="+", type=int)
+    parser.add_argument("-a", "--select-audio", metavar="TRACK", nargs="+", type=int, default=[])
+    parser.add_argument("--an", action="store_true")
+    parser.add_argument("-s", "--select-subtitle", metavar="TRACK", nargs="+", type=int, default=[])
+    parser.add_argument("--sn", action="store_true")
     parser.add_argument("-f", "--force-subtitle", metavar="TRACK", type=int)
     parser.add_argument("-h", "--help", action="store_true")
     parser.add_argument("--version", action="store_true")
@@ -70,7 +74,10 @@ def main():
         inspect_file(args.file)
         exit()
 
-    remux_file(args.file, args.select_audio, args.select_subtitle, args.force_subtitle, args.dry_run)
+    audio_tracks = args.select_audio if not args.an else None
+    subtitle_tracks = args.select_subtitle if not args.sn else None
+    forced_subtitle = args.force_subtitle if not args.sn else None
+    remux_file(args.file, audio_tracks, subtitle_tracks, forced_subtitle, args.dry_run)
 
 
 def verify_tools():
@@ -123,12 +130,12 @@ def inspect_file(file):
 def remux_file(file, audio_tracks, subtitle_tracks, forced_subtitle, dry_run):
     _, video, audio, subtitles = scan_media(file)
 
-    if audio_tracks:
+    if audio_tracks != None:
         for track in audio_tracks:
             if track <= 0 or len(audio) < track:
                 exit(f"Selected audio track out of range: {track}.")
 
-    if subtitle_tracks:
+    if subtitle_tracks != None:
         for track in subtitle_tracks:
             if track <= 0 or len(subtitles) < track:
                 exit(f"Selected subtitle track out of range: {track}")
@@ -136,8 +143,20 @@ def remux_file(file, audio_tracks, subtitle_tracks, forced_subtitle, dry_run):
     if forced_subtitle and (forced_subtitle <= 0 or len(subtitles) < forced_subtitle):
         exit(f"Forced subtitle out of range: {forced_subtitle}")
 
-    selected_audio_streams = [stream for idx, stream in enumerate(audio) if idx + 1 in audio_tracks] if audio_tracks else audio
-    selected_subtitle_streams = [stream for idx, stream in enumerate(subtitles) if idx + 1 in subtitle_tracks] if subtitle_tracks else subtitles
+    if audio_tracks == []:
+        selected_audio_streams = audio
+    elif audio_tracks == None:
+        selected_audio_streams = None
+    else:
+        selected_audio_streams = [stream for idx, stream in enumerate(audio) if idx + 1 in audio_tracks]
+    
+    if subtitle_tracks == []:
+        selected_subtitle_streams = subtitles
+    elif subtitle_tracks == None:
+        selected_subtitle_streams = None
+    else:
+        selected_subtitle_streams = [stream for idx, stream in enumerate(subtitles) if idx + 1 in subtitle_tracks]
+
     forced_subtitle_stream = subtitles[forced_subtitle - 1] if forced_subtitle else None
 
     if forced_subtitle_stream and forced_subtitle_stream not in selected_subtitle_streams:
@@ -147,44 +166,48 @@ def remux_file(file, audio_tracks, subtitle_tracks, forced_subtitle, dry_run):
     output_file = f"{title}.mkv"
 
     # thanks Don!
-    audio_arg = ""
-    for audio_stream in selected_audio_streams:
-        audio_arg += "," if audio_arg else ""
-        audio_arg += str(audio_stream["index"])
-
-    subtitle_arg = ""
+    audio_arg = []
+    if selected_audio_streams != None:
+        audio_arg += ["--audio-tracks", ",".join(map(lambda x: str(x["index"]), selected_audio_streams))]
+        first_audio_index = str(selected_audio_streams[0]["index"])
+        audio_arg += ["--default-track", first_audio_index]
+    else:
+        audio_arg = ["--no-audio"]
+    
+    subtitle_arg = []
     forced_options = []
-    for subtitle_stream in selected_subtitle_streams:
-        subtitle_arg += "," if subtitle_arg else ""
-        stream_index = str(subtitle_stream["index"])
-        subtitle_arg += stream_index
+    if selected_subtitle_streams != None:
+        subtitle_arg += ["--subtitle-tracks"]
+        for subtitle_stream in selected_subtitle_streams:
+            stream_index = str(subtitle_stream["index"])
+            subtitle_arg += [str(stream_index)]
 
-        if subtitle_stream != forced_subtitle_stream:
-            forced_options += ["--default-track", f"{stream_index}:0"]
-            forced_options += ["--forced-track", f"{stream_index}:0"]
-        else:
-            forced_options += ["--default-track", stream_index]
-            forced_options += ["--forced-track", stream_index]
+            if subtitle_stream != forced_subtitle_stream:
+                forced_options += ["--default-track", f"{stream_index}:0"]
+                forced_options += ["--forced-track", f"{stream_index}:0"]
+            else:
+                forced_options += ["--default-track", stream_index]
+                forced_options += ["--forced-track", stream_index]
+    else:
+        subtitle_arg = ["--no-subtitles"]
 
     video_index = str(video["index"])
-    first_audio_index = str(selected_audio_streams[0]["index"])
 
     mkvmerge_command = [
         "mkvmerge",
         "--output", output_file,
         "--title", "",
         "--default-track", video_index,
-        "--default-track", first_audio_index,
-    ] + forced_options + [
         "--video-tracks", video_index,
-        "--audio-tracks", audio_arg
-    ] + (["--subtitle-tracks", subtitle_arg] if subtitle_arg else []) + [
         "--no-buttons",
         "--no-attachments",
         "--no-track-tags",
-        "--no-global-tags",
-        file
-    ]
+        "--no-global-tags"]
+
+    mkvmerge_command += forced_options
+    mkvmerge_command += audio_arg
+    mkvmerge_command += subtitle_arg
+    mkvmerge_command += [file]
 
     if dry_run:
         escaped = [shlex.quote(s) for s in mkvmerge_command]
