5ffacbb6f37daf1c36e77899d2d34fd8f0ea7479
Sam H <samhutchins@users.noreply.github.com>
2024-03-06 21:49:57 +0000

    New tool: add-title-info.py.
    Useful for creating the title-info.json file that transcode.py uses

---
+++ other-tools/add-title-info.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import json
+import os.path
+from argparse import ArgumentParser
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--name", required=True)
+    parser.add_argument("--playlist", type=int, required=True)
+    parser.add_argument("--audio", type=int, nargs="+", default=[])
+    parser.add_argument("--subtitle", type=int, nargs="+", default=[])
+    parser.add_argument("--force-subtitle", type=int)
+    parser.add_argument("--crop")
+
+    args = parser.parse_args()
+
+    if os.path.exists("../title_info.json"):
+        with open("../title_info.json", "r") as f:
+            title_info = json.load(f)
+    else:
+        title_info = list()
+
+    title = dict()
+    title["name"] = args.name
+    title["playlist"] = args.playlist
+    title["audio"] = args.audio
+    title["subtitle"] = args.subtitle
+    title["forced_subtitle"] = args.force_subtitle
+    title["crop"] = args.crop
+
+    title_info.append(title)
+
+    with open("../title_info.json", "w") as f:
+        json.dump(title_info, f)
+
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
